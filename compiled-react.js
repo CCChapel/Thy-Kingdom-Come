46caff18352bc3639c57294bebaf4c26b6a895b4
Eric Rovtar <eric.rovtar@me.com>
2016-12-22 23:43:33 -0500

    Implement background color

--- compiled-react.js
+++ compiled-react.js
@@ -1,11 +1,36 @@
+// /**
+//  * Defines a background
+//  */
+// class Background extends React.Component {
+//     render() {
+//         var background = '';
+//         if (this.props.imageUrl !== undefined) {
+//             background += "background-image: " + this.props.imageUrl;
+//         }
+//         if (this.props.color !== undefined) {
+//             background += "background-color: " + this.props.color;
+//         }
+
+//         return (
+//             <div>Hi!</div>
+//         );
+//     }
+// }
+
 /**
  * Defines a section on the page
+ * @backgroundColor = background color
  */
 class Section extends React.Component {
     render() {
+        // var background = <Background color="black" />;
+        const divStyle = {
+            backgroundColor: this.props.backgroundColor
+        };
+
         return React.createElement(
             "div",
-            { "class": "section" },
+            { className: "section", style: divStyle },
             "I'm a section!"
         );
     }
@@ -19,10 +44,10 @@ class Page extends React.Component {
         return React.createElement(
             "div",
             null,
-            React.createElement(Section, null),
-            React.createElement(Section, null),
-            React.createElement(Section, null),
-            React.createElement(Section, null)
+            React.createElement(Section, { backgroundColor: "blue" }),
+            React.createElement(Section, { backgroundColor: "red" }),
+            React.createElement(Section, { backgroundColor: "yellow" }),
+            React.createElement(Section, { backgroundColor: "green" })
         );
     }
 }
--- react.js
+++ react.js
@@ -1,10 +1,37 @@
+// /**
+//  * Defines a background
+//  */
+// class Background extends React.Component {
+//     render() {
+//         var background = '';
+//         if (this.props.imageUrl !== undefined) {
+//             background += "background-image: " + this.props.imageUrl;
+//         }
+//         if (this.props.color !== undefined) {
+//             background += "background-color: " + this.props.color;
+//         }
+
+//         return (
+//             <div>Hi!</div>
+//         );
+//     }
+// }
+
 /**
  * Defines a section on the page
+ * @backgroundColor = background color
  */
 class Section extends React.Component {
     render() {
+        // var background = <Background color="black" />;
+        const divStyle = {
+            backgroundColor: this.props.backgroundColor,
+        };
+
         return (
-            <div class="section">I'm a section!</div>
+            <div className="section" style={divStyle}>
+                I'm a section!
+            </div>
         );
     }
 }
@@ -16,10 +43,10 @@ class Page extends React.Component {
     render() {
         return (
             <div>
-                <Section />
-                <Section />
-                <Section />
-                <Section />
+                <Section backgroundColor="blue" />
+                <Section backgroundColor="red" />
+                <Section backgroundColor="yellow" />
+                <Section backgroundColor="green" />
             </div>
         );
     }
