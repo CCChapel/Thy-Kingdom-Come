62536bb35cf33156956cdfe3f693d7c674d00111
Eric Rovtar <eric.rovtar@me.com>
2016-12-28 14:29:42 -0500

    Add Sample Ministry Partners JSON

--- react.js
+++ react.js
@@ -100,6 +100,8 @@ class TestModalContent extends React.Component {
     }
 }
 
+
+
 /**
  * Defines the modal section of the page
  * @show: When true, the modal is shown
@@ -190,6 +192,71 @@ class Page extends React.Component {
         );
     }
 }
+
+const MINISTRY_PARTNERS = [
+    {
+        name: 'Blessings',
+        address: '123 Street Road',
+        city: 'Hudson',
+        state: 'Ohio',
+        zipCode: '44236',
+        website: 'http://ccchapel.com',
+        options: [
+            {
+                name: 'Graduation',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+            { 
+                name: 'Annual Walk/Bike Event',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+            { 
+                name: 'Other',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+        ]
+    },
+    {
+        name: 'Broken Chains',
+        address: '123 Street Road',
+        city: 'Hudson',
+        state: 'Ohio',
+        zipCode: '44236',
+        website: 'http://ccchapel.com',
+        options: [
+            {
+                name: 'Visit Urbean Caf&eacute;',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+            { 
+                name: 'Annual Walk/Bike Event',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+            { 
+                name: 'Other',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+        ]
+    },
+    {
+        name: 'Faithful Servants',
+        address: '123 Street Road',
+        city: 'Hudson',
+        state: 'Ohio',
+        zipCode: '44236',
+        website: 'http://ccchapel.com',
+        options: [
+            {
+                name: 'Visit a Faithful Servants Clinic',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+            { 
+                name: 'Other',
+                details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
+            },
+        ]
+    },
+];
 
 ReactDOM.render(
     <Page />,
