53c5e501c107959a3acd2d98855d1b3a40dce0a1
Eric Rovtar <eric.rovtar@me.com>
2017-01-03 20:46:24 -0500

    Adjust color

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -126,7 +126,7 @@ class MinistryPartnerRow extends React.Component {
         return React.createElement(
             "div",
             {
-                className: "[ one-third ] [ bg-light-blue ] [ text-bigger cursor-point ] [ add-bottom-margin add-padding ] [ fx-bottom-border ]",
+                className: "[ one-third ] [ bg-light-blue ] [ text-bigger cursor-point ] [ add-bottom-margin add-padding ] [ fx-bottom-border fx-dark-blue ]",
                 onClick: () => this.handleClick(React.createElement(MinsitryPartnerInformation, { information: this.props.partner })) },
             this.props.partner.name
         );
--- scripts/src/components/ministryPartners/ministryPartnerRow.js
+++ scripts/src/components/ministryPartners/ministryPartnerRow.js
@@ -16,7 +16,7 @@ class MinistryPartnerRow extends React.Component {
     render() {
         return (
             <div 
-                className="[ one-third ] [ bg-light-blue ] [ text-bigger cursor-point ] [ add-bottom-margin add-padding ] [ fx-bottom-border ]"  
+                className="[ one-third ] [ bg-light-blue ] [ text-bigger cursor-point ] [ add-bottom-margin add-padding ] [ fx-bottom-border fx-dark-blue ]"  
                 onClick={() => this.handleClick(<MinsitryPartnerInformation information={this.props.partner} />)}>
                 {this.props.partner.name}
             </div>
