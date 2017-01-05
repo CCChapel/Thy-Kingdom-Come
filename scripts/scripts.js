4939252e238b7790011614479fd10225b1751e25
Eric Rovtar <eric.rovtar@me.com>
2017-01-05 14:55:04 -0500

    Add some modifiers to CTA

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -269,13 +269,41 @@ class MinistryPartnersTable extends React.Component {
 }
 /**
  * Returns a Call-to-Action button
+ * 
+ * @text = text of the button
+ * @color = color of the button (default: currentColor)
+ * @strokeColor = color of the box (default: currentColor)
  */
 class CTA extends React.Component {
     render() {
+        //Check defaults
+        var color = {
+            color: 'currentColor'
+        };
+        if (this.props.color !== undefined) {
+            color.color = this.props.color;
+        }
+
+        var stroke = {
+            stroke: 'currentColor'
+        };
+        if (this.props.strokeColor !== undefined) {
+            stroke.stroke = this.props.strokeColor;
+        }
+
         return React.createElement(
-            "a",
-            { className: "cta", href: "#" },
-            this.props.text
+            'div',
+            { className: 'cta--wrapper' },
+            React.createElement(
+                'svg',
+                { height: '60', width: '320', xmlns: 'http://www.w3.org/2000/svg' },
+                React.createElement('rect', { className: 'cta--shape', style: stroke, height: '60', width: '320' })
+            ),
+            React.createElement(
+                'div',
+                { className: 'cta--text', style: color },
+                this.props.text
+            )
         );
     }
 }
--- scripts/src/components/page/cta.js
+++ scripts/src/components/page/cta.js
@@ -1,10 +1,34 @@
 /**
  * Returns a Call-to-Action button
+ * 
+ * @text = text of the button
+ * @color = color of the button (default: currentColor)
+ * @strokeColor = color of the box (default: currentColor)
  */
 class CTA extends React.Component {
     render() {
+        //Check defaults
+        var color = {
+            color: 'currentColor'
+        };
+        if (this.props.color !== undefined) {
+            color.color = this.props.color;
+        }
+
+        var stroke = {
+            stroke: 'currentColor'
+        };
+        if (this.props.strokeColor !== undefined) {
+            stroke.stroke = this.props.strokeColor;
+        }
+
         return (
-            <a className="cta" href="#">{this.props.text}</a>
+            <div className="cta--wrapper">
+                <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
+                    <rect className="cta--shape" style={stroke} height="60" width="320" />
+                </svg>
+                <div className="cta--text" style={color}>{this.props.text}</div>
+            </div>
         );
     }
 }
