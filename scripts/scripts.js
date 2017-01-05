716735d7cc14b2adefd4b9c905975f5e8320273f
Eric Rovtar <eric.rovtar@me.com>
2017-01-05 15:59:47 -0500

    Setup Contact Form

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,48 +2,101 @@
  * Displays the Contact Form
  */
 class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        };
+
+        this.onNameChange = this.onNameChange.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onSubjectChange = this.onSubjectChange.bind(this);
+        this.onMessageChange = this.onMessageChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onNameChange(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    onEmailChange(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onSubjectChange(e) {
+        this.setState({
+            subject: e.target.value
+        });
+    }
+
+    onMessageChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        alert('Hello!');
+        e.preventDefault();
+    }
+
     render() {
-        return React.createElement(
-            "form",
-            { method: "post", action: "https://www.formstack.com/forms/index.php" },
-            React.createElement("input", { type: "hidden", name: "form", value: "2569143" }),
-            React.createElement("input", { type: "hidden", name: "viewkey", value: "jXJg3zwAgW" }),
+        return (
+            // <form method="post" action="https://www.formstack.com/forms/index.php">
             React.createElement(
-                "div",
-                null,
-                React.createElement("input", { name: "name",
-                    type: "text",
-                    placeholder: "Name",
-                    value: "" })
-            ),
-            React.createElement(
-                "div",
-                null,
-                React.createElement("input", { name: "email",
-                    type: "text",
-                    placeholder: "Email",
-                    value: "" })
-            ),
-            React.createElement(
-                "div",
-                null,
-                React.createElement("input", { name: "subject",
-                    type: "text",
-                    placeholder: "Subject",
-                    value: "" })
-            ),
-            React.createElement(
-                "div",
-                null,
-                React.createElement("textarea", { name: "message",
-                    placeholder: "Message",
-                    height: "300px",
-                    value: "" })
-            ),
-            React.createElement(
-                "div",
-                null,
-                React.createElement("input", { type: "submit", value: "Submit" })
+                'form',
+                { onSubmit: this.handleSubmit },
+                React.createElement('input', { type: 'hidden', name: 'form', value: '2569143' }),
+                React.createElement('input', { type: 'hidden', name: 'viewkey', value: 'jXJg3zwAgW' }),
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement('input', { name: 'name',
+                        type: 'text',
+                        placeholder: 'Name',
+                        value: this.state.name,
+                        onChange: this.onNameChange })
+                ),
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement('input', { name: 'email',
+                        type: 'text',
+                        placeholder: 'Email',
+                        pattern: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$',
+                        value: this.state.email,
+                        onChange: this.onEmailChange })
+                ),
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement('input', { name: 'subject',
+                        type: 'text',
+                        placeholder: 'Subject',
+                        value: this.state.subject,
+                        onChange: this.onSubjectChange })
+                ),
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement('textarea', { name: 'message',
+                        placeholder: 'Message',
+                        height: '300px',
+                        value: this.state.message,
+                        onChange: this.onMessageChange })
+                ),
+                React.createElement(
+                    'div',
+                    null,
+                    React.createElement('input', { type: 'submit', value: 'Submit' })
+                )
             )
         );
     }
--- scripts/src/components/forms/contact.js
+++ scripts/src/components/forms/contact.js
@@ -2,9 +2,55 @@
  * Displays the Contact Form
  */
 class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        };
+
+        this.onNameChange = this.onNameChange.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onSubjectChange = this.onSubjectChange.bind(this);
+        this.onMessageChange = this.onMessageChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onNameChange(e) {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    onEmailChange(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onSubjectChange(e) {
+        this.setState({
+            subject: e.target.value
+        });
+    }
+
+    onMessageChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        alert('Hello!');
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <form method="post" action="https://www.formstack.com/forms/index.php">
+            // <form method="post" action="https://www.formstack.com/forms/index.php">
+            <form onSubmit={this.handleSubmit}>
                 <input type="hidden" name="form" value="2569143" />
                 <input type="hidden" name="viewkey" value="jXJg3zwAgW" />
 
@@ -12,28 +58,33 @@ class ContactForm extends React.Component {
                     <input name="name"
                         type="text" 
                         placeholder="Name"
-                        value=""  />
+                        value={this.state.name}
+                        onChange={this.onNameChange} />
                 </div>
 
                 <div>
                     <input name="email"
                         type="text" 
                         placeholder="Email"
-                        value="" />
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
+                        value={this.state.email}
+                        onChange={this.onEmailChange} />
                 </div>
 
                 <div>
                     <input name="subject"
                         type="text" 
                         placeholder="Subject"
-                        value="" />
+                        value={this.state.subject}
+                        onChange={this.onSubjectChange} />
                 </div>
 
                 <div>
                     <textarea name="message"
                         placeholder="Message"
                         height="300px"
-                        value="" />
+                        value={this.state.message}
+                        onChange={this.onMessageChange} />
                 </div>
 
                 <div>
