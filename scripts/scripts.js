442147b4f3d80bbaa2db3fb7984f03e50ed2eeb4
Eric Rovtar <eric.rovtar@me.com>
2017-01-04 19:18:58 -0500

    Move Math functions to its own file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -391,6 +391,40 @@ class Section extends React.Component {
         );
     }
 }
+/**
+ * Converts from degrees to radians.
+ * 
+ * @degrees = degrees to convert to radians
+ **/
+Math.radians = function (degrees) {
+  return degrees * Math.PI / 180;
+};
+
+/**
+ * Converts from radians to degrees.
+ * 
+ * @radians = radians to convert to degrees
+ **/
+Math.degrees = function (radians) {
+  return radians * 180 / Math.PI;
+};
+
+/**
+ * Finds a point on a circle
+ * 
+ * @cx = x coordinate of center of circle
+ * @cy = y coordinate of the center of the circle
+ * @r = radius of circle
+ * @a = angle to find point at
+ * 
+ * RETURNS an object with an x property and y property
+ **/
+Math.pointOnCircle = function (cx, cy, r, a) {
+  return {
+    x: cx + r * Math.cos(a),
+    y: cy + r * Math.sin(a)
+  };
+};
 const MINISTRY_PARTNERS = [{
     name: 'Blessings',
     address: '123 Street Road',
@@ -440,38 +474,5 @@ const MINISTRY_PARTNERS = [{
         details: 'Integer imperdiet ullamcorper libero, eget consequat lectus feugiat nec. Cras condimentum, nulla nec convallis interdum, dui nisl iaculis nisl, sed dignissim ligula neque eu turpis. Suspendisse potenti.'
     }]
 }];
-
-/**
- * Converts from degrees to radians.
- * 
- * @degrees = degrees to convert to radians
- **/
-Math.radians = function (degrees) {
-    return degrees * Math.PI / 180;
-};
-
-/**
- * Converts from radians to degrees.
- * 
- * @radians = radians to convert to degrees
- **/
-Math.degrees = function (radians) {
-    return radians * 180 / Math.PI;
-};
-
-/**
- * Finds a point on a circle
- * 
- * @cx = x coordinate of center of circle
- * @cy = y coordinate of the center of the circle
- * @r = radius of circle
- * @a = angle to find point at
- **/
-Math.pointOnCircle = function (cx, cy, r, a) {
-    return {
-        x: cx + r * Math.cos(a),
-        y: cy + r * Math.sin(a)
-    };
-};
 
 ReactDOM.render(React.createElement(Page, null), document.getElementById('root'));
---
+++ scripts/src/includes/math.js
@@ -0,0 +1,34 @@
+/**
+ * Converts from degrees to radians.
+ * 
+ * @degrees = degrees to convert to radians
+ **/
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+ 
+/**
+ * Converts from radians to degrees.
+ * 
+ * @radians = radians to convert to degrees
+ **/
+Math.degrees = function(radians) {
+  return radians * 180 / Math.PI;
+};
+
+/**
+ * Finds a point on a circle
+ * 
+ * @cx = x coordinate of center of circle
+ * @cy = y coordinate of the center of the circle
+ * @r = radius of circle
+ * @a = angle to find point at
+ * 
+ * RETURNS an object with an x property and y property
+ **/
+Math.pointOnCircle = function(cx, cy, r, a) {
+    return {
+        x: cx + r * Math.cos(a),
+        y: cy + r * Math.sin(a)
+    }
+};
