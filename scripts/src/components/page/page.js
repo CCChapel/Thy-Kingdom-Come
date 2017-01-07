442878dce30316f63bddc735b0411fe67800ca04
Eric Rovtar <eric.rovtar@me.com>
2017-01-07 10:22:51 -0500

    Re-enable LO Spree Content

--- scripts/src/components/page/page.js
+++ scripts/src/components/page/page.js
@@ -61,7 +61,6 @@ class Page extends React.Component {
                     </div>
                 </Section>
 
-                {/*
                 <Section className="bg-medium-blue">
                     <div className="content-wrapper">
                         <div className="lock-width center-by-margin">
@@ -88,7 +87,6 @@ class Page extends React.Component {
                         <MinistryPartnersTable showModal={this.showModal} partners={MINISTRY_PARTNERS} />
                     </div>
                 </Section>
-                */}
 
                 <Section className="bg-dark-blue text-white">
                     <div className="content-wrapper">
