cb75dc1a3b9d43a9e7365f3a06cfa3a3a432f60d
Eric Rovtar <eric.rovtar@me.com>
2017-01-09 11:44:43 -0500

    Create Vimeo Component

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,25 @@
 /**
+ * Embeds a Vimeo video
+ * 
+ * @vimeoId = id of the Vimeo video to embed
+ * @className = classes to apply to iframe
+ * @width = width of the iframe
+ * @height = height of the iframe
+ */
+class VimeoVideo extends React.Component {
+    render() {
+        var src = String.format("https://player.vimeo.com/video/{0}?color=28708a&title=0&byline=0&portrait=0", [this.props.vimeoID]);
+        return React.createElement("iframe", { src: src,
+            className: this.props.className,
+            width: this.props.width,
+            height: this.props.height,
+            frameborder: "0",
+            webkitallowfullscreen: true,
+            mozallowfullscreen: true,
+            allowfullscreen: true });
+    }
+}
+/**
  * Displays the Contact Form
  * 
  * @className = Class Names to apply to root form element
---
+++ scripts/src/components/embeds/vimeo.js
@@ -0,0 +1,25 @@
+/**
+ * Embeds a Vimeo video
+ * 
+ * @vimeoId = id of the Vimeo video to embed
+ * @className = classes to apply to iframe
+ * @width = width of the iframe
+ * @height = height of the iframe
+ */
+class VimeoVideo extends React.Component {
+    render() {
+        var src = String.format(
+            "https://player.vimeo.com/video/{0}?color=28708a&title=0&byline=0&portrait=0",
+            [ this.props.vimeoID ]);
+        return (
+            <iframe src={src} 
+                className={this.props.className}
+                width={this.props.width} 
+                height={this.props.height}
+                frameborder="0" 
+                webkitallowfullscreen 
+                mozallowfullscreen 
+                allowfullscreen></iframe>
+        );
+    }
+}
