262e5645283b24791dfbf1694c5565d3510a67bb
Eric Rovtar <eric.rovtar@me.com>
2017-01-10 19:46:48 -0500

    Move ministry partners out of state

--- scripts/src/components/ministryPartners/ministryPartnerTable.js
+++ scripts/src/components/ministryPartners/ministryPartnerTable.js
@@ -5,23 +5,61 @@ class MinistryPartnersTable extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            ministryPartners: []
-        };
+        // this.state = {
+        //     ministryPartners: []
+        // };
 
         this.handleClick = this.handleClick.bind(this);
-        this.loadMinistryPartners = this.loadMinistryPartners.bind(this);
+        // this.loadMinistryPartners = this.loadMinistryPartners.bind(this);
     }
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     //Load Ministry Partners
+    //     // var ministryPartners = new Array();
+    //     var url = "http://sandbox.ccchapel.com/Thy-Kingdom-Come/data/outreach-spree.json";
+    //     var request = new Request(url, {
+    //             method: 'get',
+    //             mode: 'no-cors'
+    //         });
+    //     var loadMinistryPartners = this.loadMinistryPartners;
+        
+    //     fetch(request)
+    //         // .then(function status(response) {  
+    //         //     if (response.status >= 200 && response.status < 300) {  
+    //         //         return Promise.resolve(response)  
+    //         //     } else {  
+    //         //         return Promise.reject(new Error(response.statusText))  
+    //         //     }  
+    //         // })
+    //         .then(function json(response) {  
+    //             return response.json()  
+    //         })
+    //         .then(function(data) {
+    //             loadMinistryPartners(data);
+    //             console.log('Request succeeded with JSON response', data);
+    //         }).catch(function(error) {
+    //             console.log('Request failed', error);
+    //         });
+
+    //     console.log(this.state.ministryPartners);
+    // }
+
+    handleClick(content) {
+        this.props.showModal(content);
+    }
+
+    // loadMinistryPartners(data) {
+    //     this.setState({ minsitryPartners: data });
+    // }
+
+    render() {
         //Load Ministry Partners
-        // var ministryPartners = new Array();
+        var ministryPartners = new Array();
         var url = "http://sandbox.ccchapel.com/Thy-Kingdom-Come/data/outreach-spree.json";
         var request = new Request(url, {
                 method: 'get',
                 mode: 'no-cors'
             });
-        var loadMinistryPartners = this.loadMinistryPartners;
         
         fetch(request)
             // .then(function status(response) {  
@@ -35,24 +73,14 @@ class MinistryPartnersTable extends React.Component {
                 return response.json()  
             })
             .then(function(data) {
-                loadMinistryPartners(data);
-                console.log('Request succeeded with JSON response', data);
+                ministryPartners = data;
+                console.log('Request succeeded with JSON response', ministryPartners);
             }).catch(function(error) {
                 console.log('Request failed', error);
             });
 
-        console.log(this.state.ministryPartners);
-    }
+        console.log(ministryPartners);
 
-    handleClick(content) {
-        this.props.showModal(content);
-    }
-
-    loadMinistryPartners(data) {
-        this.setState({ minsitryPartners: data });
-    }
-
-    render() {
         //Create container for rows
         var rows = [];
 
